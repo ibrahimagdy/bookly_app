adc3e066b0d335fdab86d9c572a8efe38e6198fe
ibrahimagdy <iibbrraahhimm00@gmail.com>
2024-09-04 21:51:43 +0300

    adding animation to splash

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,33 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget{
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    animation = Tween<Offset>(
+      begin: const Offset(0, -7),
+      end: const Offset(0, 0),
+    ).animate(animationController);
+
+    animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +35,10 @@ class SplashViewBody extends StatelessWidget{
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(Assets.logo),
+        SlideTransition(
+          position: animation,
+          child: Image.asset(Assets.logo),
+        ),
       ],
     );
   }
