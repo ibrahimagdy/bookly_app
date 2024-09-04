0b66bfd6db101ce5d1d050b2e60b5bce4bb35514
ibrahimagdy <iibbrraahhimm00@gmail.com>
2024-09-05 00:24:38 +0300

    adding custom app bar adding book item

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,11 @@
-import 'package:bookly_app/core/utils/constants.dart';
-import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:bookly_app/bookly_app.dart';
+import 'package:bookly_app/core/routing/app_routes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: kPrimaryColor,
-      ),
-      home: const SplashView(),
-    );
-  }
-
+  runApp(
+    BooklyApp(
+      appRoutes: AppRoutes(),
+    ),
+  );
 }
