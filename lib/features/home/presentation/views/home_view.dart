332fd554fd81838afcd9400d95c44f24f963da0c
ibrahimagdy <iibbrraahhimm00@gmail.com>
2024-09-08 17:43:27 +0300

    display featured books from api

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,8 +1,13 @@
+import 'package:bookly_app/core/di/dependecy_injection.dart';
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/data/repo/home_repo_impl.dart';
+import 'package:bookly_app/features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/features/home/presentation/view_models/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/newest_book_list_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeView extends StatelessWidget {
@@ -23,7 +28,11 @@ class HomeView extends StatelessWidget {
                   children: [
                     const CustomAppBar(),
                     SizedBox(height: 23.h),
-                    const FeaturedBookListView(),
+                    BlocProvider(
+                        create: (context) =>
+                            FeaturedBooksCubit(getIt.get<HomeRepoImpl>())
+                              ..fetchFeaturedBooks(),
+                        child: const FeaturedBookListView()),
                     SizedBox(height: 49.h),
                     Text(
                       'Newest Books',
@@ -33,7 +42,11 @@ class HomeView extends StatelessWidget {
                   ],
                 ),
               ),
-              const BestSellerBookListView(),
+              BlocProvider(
+                create: (context) => NewestBooksCubit(getIt.get<HomeRepoImpl>())
+                  ..fetchNewestBooks(),
+                child: const NewestBookListView(),
+              ),
             ],
           ),
         ),
--- lib/features/home/presentation/views/widgets/featured_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_books_list_view.dart
@@ -1,20 +1,42 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/core/widgets/custom_circle_progress_indicator.dart';
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_item_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class FeaturedBookListView extends StatelessWidget {
   const FeaturedBookListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: ListView.builder(
-        itemCount: 10,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const BookItemImage();
-        },
-      ),
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.3,
+            child: ListView.builder(
+              itemCount: state.books.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding:  EdgeInsets.symmetric(horizontal: 10.w),
+                  child: BookItemImage(
+                    imageUrl: state.books[index].volumeInfo.imageLinks?.thumbnail ?? Assets.testImage,
+                  ),
+                );
+              },
+            ),
+          );
+        } else if (state is FeaturedBooksFailure) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        }
+        else{
+          return const CustomCircleProgressIndicator();
+        }
+      },
     );
   }
 }
