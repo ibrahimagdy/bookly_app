8a8dfa59ce72032e4cb6b8513618578e24a4f8a4
ibrahimagdy <iibbrraahhimm00@gmail.com>
2024-09-09 00:35:39 +0300

    display newest books from api

--- lib/features/home/presentation/views/widgets/newest_book_list_view.dart
+++ lib/features/home/presentation/views/widgets/newest_book_list_view.dart
@@ -1,5 +1,9 @@
+import 'package:bookly_app/core/widgets/custom_circle_progress_indicator.dart';
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/features/home/presentation/view_models/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/newest_book_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NewestBookListView extends StatelessWidget {
@@ -7,14 +11,30 @@ class NewestBookListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverList(
-      delegate: SliverChildBuilderDelegate(
-        childCount: 14,
-        (context, index) => Padding(
-          padding: EdgeInsets.symmetric(vertical: 8.h),
-          child: const BestSellerBookItem(),
-        ),
-      ),
+    return BlocBuilder<NewestBooksCubit, NewestBooksState>(
+      builder: (context, state) {
+        if (state is NewestBooksSuccess) {
+          return SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.h),
+                child: NewestBookItem(
+                  bookModel: state.books[index],
+                ),
+              ),
+              childCount: state.books.length,
+            ),
+          );
+        } else if (state is NewestBooksFailure) {
+          return SliverToBoxAdapter(
+            child: CustomErrorWidget(errMessage: state.errMessage),
+          );
+        } else {
+          return const SliverToBoxAdapter(
+            child: CustomCircleProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
